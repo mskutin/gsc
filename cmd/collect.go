34319c9bc01183e626a210af3d2fa4f09698a071
Maksim Skutin <maksim.skutin@outlook.com>
2020-07-07 23:22:36 +0800

    Add tsv and csv formatter

--- cmd/collect.go
+++ cmd/collect.go
@@ -11,6 +11,7 @@ import (
 )
 
 var repos []string
+var format string
 
 type Repository struct {
 	name             string
@@ -34,52 +35,68 @@ Collect statistics for multiple repositories:
 		-r helm/charts \
 		-r github/hubot
 	3) echo "helm/charts,mskutin/gsc" | xargs gsc collect -r
+
 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		//TODO: Validation
 		//TODO: Distinguish CSV & TSV
-		var repositories []Repository
-		for _, repo := range repos {
-			params := strings.Split(repo, "/")
-
-			head, err := gh.GetHead(params[0], params[1])
-			if err != nil {
-				log.Fatalln("Unable to retrieve head.", err)
-			}
-			details, err := gh.GetRepository(params[0], params[1])
-			if err != nil {
-				log.Fatalln("Unable to retrieve repository details.", err)
-			}
-			repositories = append(repositories, Repository{
-				name:             details.FullName,
-				cloneURL:         details.CloneURL,
-				lastCommitAuthor: head.Commit.Author.Name,
-				lastCommitDate:   head.Commit.Author.Date.UTC().String(),
-				defaultBranch:    details.DefaultBranch,
-			})
+		var separator rune
+		stats := getStats()
+		switch format {
+		case "tsv":
+			separator = '\t'
+		default:
+			separator = ','
 		}
-		PrintCSV(repositories)
+		PrintCSV(stats, separator)
 	},
 }
 
-func PrintCSV(repos []Repository) {
+func getStats() []Repository {
+	var repositories []Repository
+	for _, repo := range repos {
+		params := strings.Split(repo, "/")
+
+		head, err := gh.GetHead(params[0], params[1])
+		if err != nil {
+			log.Fatalln("Unable to retrieve head.", err)
+		}
+		details, err := gh.GetRepository(params[0], params[1])
+		if err != nil {
+			log.Fatalln("Unable to retrieve repository details.", err)
+		}
+		repositories = append(repositories, Repository{
+			name:             details.FullName,
+			cloneURL:         details.CloneURL,
+			lastCommitAuthor: head.Commit.Author.Name,
+			lastCommitDate:   head.Commit.Author.Date.UTC().String(),
+			defaultBranch:    details.DefaultBranch,
+		})
+	}
+	return repositories
+}
+
+func PrintCSV(repos []Repository, comma rune) {
 	records := [][]string{{"name", "clone_url", "last_commit_author", "last_commit_date"}}
 	for _, repo := range repos {
 		row := []string{repo.name, repo.cloneURL, repo.lastCommitAuthor, repo.lastCommitDate}
 		records = append(records, row)
 	}
 	w := csv.NewWriter(os.Stdout)
+	w.Comma = comma
 	err := w.WriteAll(records)
 	if err != nil {
 		log.Fatalln("error writing csv", err)
 	}
-
-	if err := w.Error(); err != nil {
-		log.Fatalln("error writing csv:", err)
-	}
 }
 func init() {
 	rootCmd.AddCommand(collectCmd)
+	collectCmd.Flags().StringVarP(
+		&format,
+		"format",
+		"f",
+		"csv",
+		"--format tsv")
 	collectCmd.Flags().
 		StringSliceVarP(
 			&repos,
